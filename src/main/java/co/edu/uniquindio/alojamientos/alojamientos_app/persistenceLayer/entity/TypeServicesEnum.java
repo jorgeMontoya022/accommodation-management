d2140a9136f07141c7cde51827fba64968463987
rimak1 <sotoperezjuanesteban@gmail.com>
2025-11-12 19:50:34 -0500

    Se añaden más servicios al enum
    Se añaden más servicios al enum

--- src/main/java/co/edu/uniquindio/alojamientos/alojamientos_app/persistenceLayer/entity/TypeServicesEnum.java
+++ src/main/java/co/edu/uniquindio/alojamientos/alojamientos_app/persistenceLayer/entity/TypeServicesEnum.java
@@ -1,5 +1,17 @@
 package co.edu.uniquindio.alojamientos.alojamientos_app.persistenceLayer.entity;
 
 public enum TypeServicesEnum {
-    WIFI, PETS, PARKING, POOL, KITCHEN, AIR_CONDITIONING
+    WIFI,
+    AIR_CONDITIONING,
+    KITCHEN,
+    TV,
+    WASHER,
+    PARKING,
+    GARDEN,
+    POOL,
+    GYM,
+    SPA,
+    BBQ,
+    TERRACE,
+    PETS
 }
